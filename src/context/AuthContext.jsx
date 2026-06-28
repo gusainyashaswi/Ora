414cefc138b559f3de33346eddfc79579af7220f
Yashaswi Gusain <gusainyashaswi@gmail.com>
2026-06-28 21:12:12 +0530

    feat: add AuthContext and AuthProvider for authentication management

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem("isLoggedIn") === "true"
+  );
+
+  function login() {
+    localStorage.setItem("isLoggedIn", "true");
+    setIsLoggedIn(true);
+  }
+
+  function logout() {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthContext;
